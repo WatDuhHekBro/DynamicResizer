9a4d016010c4ecc0a2d6c81cc21d3773f814c099
WatDuhHekBro <44940783+WatDuhHekBro@users.noreply.github.com>
2020-09-16 18:50:09 -0500

    Barebones prototype version

---
+++ resizer.js
@@ -0,0 +1,26 @@
+// This is the core operation that resizes windows.
+// Also, for whatever reason, you have to have a background page even if it's completely empty. Otherwise, you won't have access to the "chrome" variable.
+function resize(width, height, offsetX, offsetY)
+{
+	chrome.windows.getCurrent(win => {
+		chrome.windows.update(win.id, {
+			top: offsetY,
+			left: offsetX,
+			width: width,
+			height: height
+		});
+	});
+}
+
+// Extension popups don't allow inline JavaScript, so onclick is useless in the document.
+const eventTable = {
+	youtube: [512, 453, 620, 325],
+	crunchyroll: [692, 498, 535, 310]
+};
+
+for(const id in eventTable)
+{
+	const element = document.getElementById(id);
+	const args = eventTable[id];
+	element.onclick = () => resize(...args);
+}
