d6f442cd942132d155c49e2aaeec6e36cbc26331
WatDuhHekBro <44940783+WatDuhHekBro@users.noreply.github.com>
2021-02-22 16:06:45 -0600

    Attempted to setup configuration for TypeScript

--- src/resizer.ts
+++ src/resizer.ts
@@ -1,8 +1,9 @@
+declare var chrome: any; // no idea on how to fix this
+
 // This is the core operation that resizes windows.
 // Also, for whatever reason, you have to have a background page even if it's completely empty. Otherwise, you won't have access to the "chrome" variable.
-function resize(width, height, offsetX, offsetY)
-{
-	chrome.windows.getCurrent(win => {
+function resize(width: number, height: number, offsetX: number, offsetY: number) {
+	chrome.windows.getCurrent((win: any) => {
 		chrome.windows.update(win.id, {
 			top: offsetY,
 			left: offsetX,
@@ -13,14 +14,13 @@ function resize(width, height, offsetX, offsetY)
 }
 
 // Extension popups don't allow inline JavaScript, so onclick is useless in the document.
-const eventTable = {
+const eventTable: {[website: string]: [number, number, number, number]} = {
 	youtube: [512, 448, 650, 345],
 	crunchyroll: [692, 498, 535, 310]
 };
 
-for(const id in eventTable)
-{
+for (const id in eventTable) {
 	const element = document.getElementById(id);
 	const args = eventTable[id];
-	element.onclick = () => resize(...args);
+	element!.onclick = () => resize(...args);
 }
